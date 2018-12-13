cc6ce1c4c327f776c45aedb4c848d90bb3e4d72f
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-14 00:18:20 +0400

    palindromeRearranging

---
+++ Intro/palindromeRearranging/palindromeRearranging/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace palindromeRearranging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abbcabb";
+            Console.WriteLine(palindromeRearranging(str));
+        }
+
+        static bool palindromeRearranging(string inputString)
+        {
+            if (inputString.Length % 2 == 0 && inputString.Distinct().Any(c => inputString.Count(x => x == c) % 2 != 0))
+                return false;
+            if (inputString.Length % 2 == 1)
+            {
+                int count = 0;
+                foreach (var c in inputString.Distinct())
+                {
+                    if (inputString.Count(x => x == c) % 2 != 0)
+                        count++;
+                    if (count > 1)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
