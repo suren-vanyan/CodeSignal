7f4f5009c61bb1b40ee083cd25f3a8ef449e4133
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-15 20:29:00 +0400

    avoidObstacles

---
+++ Intro/avoidObstacles/avoidObstacles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeSignal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 1, 4, 10, 6, 2 };
+            Console.WriteLine(avoidObstacles(inputArray));
+        }
+
+        static int avoidObstacles(int[] inputArray)
+        {
+            int jumpLength = 1;
+            bool jumping = false;
+            while (!jumping)
+            {
+                jumping = true;
+                ++jumpLength;
+                foreach (var item in inputArray)
+                {
+                    if (item % jumpLength == 0)
+                    {
+                        jumping = false;
+                        continue;
+                    }
+                }
+            }
+
+            return jumpLength;
+        }
+    }
+}
