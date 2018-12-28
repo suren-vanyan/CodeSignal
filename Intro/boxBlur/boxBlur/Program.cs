3fb0a356ba600de630105aad05d4484425a672cf
suren-vanyan <vanyansuren@gmail.com>
2018-12-28 20:30:48 +0400

    add description

--- Intro/boxBlur/boxBlur/Program.cs
+++ Intro/boxBlur/boxBlur/Program.cs
@@ -4,6 +4,15 @@ namespace boxBlur
 {
     class Program
     {
+        //Last night you partied a little too hard.Now there's a black and white photo of you that's
+        //about to go viral! You can't let this ruin your reputation,
+        //so you want to apply the box blur algorithm to the photo to hide its content.
+
+         //The pixels in the input image are represented as integers.
+         //The algorithm distorts the input image in the following way:
+         //Every pixel x in the output image has a value equal to the average 
+         //value of the pixel values from the 3 × 3 square that has its center at x,
+         //including x itself.All the pixels on the border of x are then removed.
         static void Main(string[] args)
         {
             int[][] image = {new int[]{7,4,0,1 },
