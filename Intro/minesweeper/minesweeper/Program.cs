8555be528761e40b2f7f6269a99946d111f2599a
suren-vanyan <vanyansuren@gmail.com>
2018-12-29 00:13:38 +0400

    add description

--- Intro/minesweeper/minesweeper/Program.cs
+++ Intro/minesweeper/minesweeper/Program.cs
@@ -2,6 +2,12 @@
 
 namespace minesweeper
 {
+    //In the popular Minesweeper game you have a board 
+    //with some mines and those cells that don't contain 
+    //a mine have a number in it that indicates the total 
+    //number of mines in the neighboring cells. 
+    //Starting off with some arrangement of mines we
+    //want to create a Minesweeper game setup.
     class Program
     {
         static void Main(string[] args)
