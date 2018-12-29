b829cf9baebaa714fab0ba43191b88c998205b56
suren-vanyan <vanyansuren@gmail.com>
2018-12-29 19:27:24 +0400

    add task variableName

---
+++ Intro/variableName/variableName/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeSignal
+{
+    //Correct variable names consist only of English letters,
+    //digits and underscores and they can't start with a digit.
+    //Check if the given string is a correct variable name.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "var_1__Int";
+            name = Console.ReadLine();
+            Console.WriteLine(variableName(name));
+
+        }
+
+        static bool variableName(string name)
+        {
+
+            string pattern = @"^\d";
+            var regex = Regex.Match(name, pattern);
+            if (regex.Success)
+                return false;
+            else
+                return name.All(c => (char.IsLetterOrDigit(c)) || c == '_');
+        }
+
+    }
+}
