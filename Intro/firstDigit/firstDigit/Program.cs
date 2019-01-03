9e9fb5b6c414083c66ebe86fbf63ef42bcebe286
suren-vanyan <vanyansuren@gmail.com>
2019-01-03 14:59:08 +0400

    Find the leftmost digit that occurs in a given string.

---
+++ Intro/firstDigit/firstDigit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace firstDigit
+{
+    class Program
+    {
+        //Find the leftmost digit that occurs in a given string.
+        static void Main(string[] args)
+        {
+            string inputString = "var_1__Int";
+            Console.WriteLine(firstDigit(inputString));
+        }
+
+        static char firstDigit(string inputString)
+        {
+            var isDigit = false;
+            foreach (char s in inputString)
+            {
+
+                isDigit = int.TryParse(s.ToString(), out int digit);
+
+                if (isDigit)
+                    return s;
+            }
+            return default(char);
+        }
+
+    }
+}
