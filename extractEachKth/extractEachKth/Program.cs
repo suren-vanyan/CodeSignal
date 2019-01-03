6d558ac394823c011a1c60708fcab0923fe332b2
suren-vanyan <vanyansuren@gmail.com>
2019-01-03 14:34:18 +0400

    add extractEachKth

---
+++ extractEachKth/extractEachKth/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace extractEachKth
+{
+    class Program
+    {
+        //Given array of integers, remove each kth element from it.
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 1, 2, 1, 2, 1, 2 };
+            foreach (var item in extractEachKth(inputArray, 2))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[] extractEachKth(int[] inputArray, int k)
+        {
+
+            List<int> newArray = new List<int>();
+            for (int i = 0, temp = 0; i < inputArray.Length; i++)
+            {
+                if (temp + 1 != k)
+                {
+                    newArray.Add(inputArray[i]);
+                    temp++;
+                }
+                else
+                    temp = 0;
+
+            }
+
+            return newArray.ToArray();
+        }
+    }
+}
