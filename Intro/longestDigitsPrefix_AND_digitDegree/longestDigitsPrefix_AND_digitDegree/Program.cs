d4d93d2b77a2218d85633dc7090847075c8568c6
suren-vanyan <vanyansuren@gmail.com>
2019-01-05 00:52:42 +0400

    Task 40 and Task 41

---
+++ Intro/longestDigitsPrefix_AND_digitDegree/longestDigitsPrefix_AND_digitDegree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace longestDigitsPrefix_AND_digitDegree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 91;
+            Console.WriteLine(digitDegree(n));
+
+            string inputString = "12abc34";
+            Console.WriteLine(longestDigitsPrefix(inputString));
+        }
+
+        static int digitDegree(int n)
+        {
+            string temp = n.ToString();
+            int i = 0;
+            while (n > 9)
+            {
+                i++;
+                n = temp.Select(o => (int)char.GetNumericValue(o)).Sum();
+                temp = n.ToString();
+            }
+
+            return i;
+        }
+
+
+        static string longestDigitsPrefix(string inputString)
+        {
+            Match regex = Regex.Match(inputString, @"^(\d)+");
+            return regex.ToString();
+
+        }
+    }
+}
