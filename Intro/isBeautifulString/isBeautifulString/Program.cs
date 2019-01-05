4e30086318e276f8710f0755aaa49e6eff888286
suren-vanyan <vanyansuren@gmail.com>
2019-01-05 20:55:34 +0400

    add isBeautifulString

---
+++ Intro/isBeautifulString/isBeautifulString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace isBeautifulString
+{
+     // A string is said to be beautiful if b occurs in it 
+    //no more times than a; c occurs in it no more times than b; etc.
+    //Given a string, check whether it is beautiful.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "aabbb";
+            Console.WriteLine(isBeautifulString(inputString));
+        }
+
+        static bool isBeautifulString(string inputString)
+        {
+
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            List<int> occurs = new List<int>();
+            int temp = 0;
+            foreach (var item in alphabet)
+            {
+                temp = inputString.Count(i => i == item);
+                occurs.Add(temp);
+            }
+
+            List<int> occursByDecending = new List<int>();
+            occursByDecending = occurs.OrderByDescending(i => i).ToList();
+            for (int i = 0; i < occurs.Count; i++)
+            {
+                if (occurs[i] != occursByDecending[i])
+                    return false;
+            }
+
+            return true;
+
+        }
+    }
+}
