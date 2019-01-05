a8dbeef3578544812a950af3fe34b0b25e0dcb04
suren-vanyan <vanyansuren@gmail.com>
2019-01-05 20:52:04 +0400

    add bishopAndPawn

---
+++ Intro/bishopAndPawn/bishopAndPawn/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace bishopAndPawn
+{
+    //Given the positions of a white bishop and a black pawn on the standard chess board,
+    //determine whether the bishop can capture the pawn in one move.
+    //The bishop has no restrictions in distance for each move, 
+    //but is limited to diagonal movement. Check out the example below to see how it can move:
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bishop = "a5"; string pawn = "c3";
+            Console.WriteLine(bishopAndPawn(bishop, pawn));
+        }
+
+        static bool bishopAndPawn(string bishop, string pawn)
+        {
+
+            double subFirstCoord = bishop[0] > pawn[0] ? bishop[0] - pawn[0] : pawn[0] - bishop[0];
+            double subSecondCoord = char.GetNumericValue(bishop[1]) - char.GetNumericValue(pawn[1]);
+            if (subSecondCoord < 0)
+                subSecondCoord *= -1;
+            if (subFirstCoord == subSecondCoord)
+                return true;
+            return false;
+
+        }
+    }
+}
