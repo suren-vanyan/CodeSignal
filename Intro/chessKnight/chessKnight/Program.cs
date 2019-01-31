8714614bc59d7d3ef37749069d02c442018b9542
suren-vanyan <vanyansuren@gmail.com>
2019-01-31 23:57:25 +0400

    chessKnight

---
+++ Intro/chessKnight/chessKnight/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace chessKnight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cell = "a1";
+
+            Console.WriteLine(chessKnight(cell));
+
+        }
+
+        static int chessKnight(string cell)
+        {
+            int moveCount = 0;
+            if (cell[0] + 2 <= 'h' && char.GetNumericValue(cell.ToString(), 1) + 1 <= 8)
+                moveCount++;
+            if (cell[0] + 2 <= 'h' && char.GetNumericValue(cell.ToString(), 1) - 1 >= 1)
+                moveCount++;
+            if (cell[0] + 1 <= 'h' && char.GetNumericValue(cell.ToString(), 1) - 2 >= 1)
+                moveCount++;
+            if (cell[0] - 1 >= 'a' && char.GetNumericValue(cell.ToString(), 1) - 2 >= 1)
+                moveCount++;
+            if (cell[0] - 2 >= 'a' && char.GetNumericValue(cell.ToString(), 1) + 1 <= 8)
+                moveCount++;
+            if (cell[0] - 2 >= 'a' && char.GetNumericValue(cell.ToString(), 1) - 1 >= 1)
+                moveCount++;
+            if (cell[0] - 1 >= 'a' && char.GetNumericValue(cell.ToString(), 1) + 2 <= 8)
+                moveCount++;
+            if (cell[0] + 1 <= 'h' && char.GetNumericValue(cell.ToString(), 1) + 2 <= 8)
+                moveCount++;
+
+            return moveCount;
+        }
+    }
+}
