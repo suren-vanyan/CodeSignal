59c6f794de7b1e9c58f0d4d0231542b933bd6f0a
suren-vanyan <vanyansuren@gmail.com>
2019-01-31 23:51:50 +0400

    isDigit

---
+++ Intro/isDigit/isDigit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace isDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = 'a';
+            Console.WriteLine(isDigit(symbol));
+        }
+
+        static bool isDigit(char symbol)
+        {
+           bool result= char.IsDigit(symbol);
+            return result;
+            //double digit = char.GetNumericValue(symbol);
+            //if (digit != -1.0)
+            //    return true;
+            //return false;
+        }
+    }
+}
