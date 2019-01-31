5667ae830f345b118332c8c2f35b37dc15cfe182
suren-vanyan <vanyansuren@gmail.com>
2019-01-31 23:56:22 +0400

    description

--- Intro/lineEncoding/lineEncoding/Program.cs
+++ Intro/lineEncoding/lineEncoding/Program.cs
@@ -4,6 +4,14 @@ namespace lineEncoding
 {
     class Program
     {
+        //Given a string, return its encoding defined as follows:
+        // First, the string is divided into the least possible number 
+        //of disjoint substrings consisting of identical characters
+        //for example, "aabbbc" is divided into["aa", "bbb", "c"]
+        //Next, each substring with length greater than one is replaced with a 
+        //concatenation of its length and the repeating character
+        //for example, substring "bbb" is replaced by "3b"
+        //Finally, all the new strings are concatenated together in the same order and a new string is returned
         static void Main(string[] args)
         {
             string s = "ssiiggkooo";
