fcb0f7510d74700d4f0f8c9daf170196c9255b7a
suren-vanyan <vanyansuren@gmail.com>
2019-02-01 00:16:27 +0400

    description

--- Intro/chessKnight/chessKnight/Program.cs
+++ Intro/chessKnight/chessKnight/Program.cs
@@ -4,6 +4,13 @@ namespace chessKnight
 {
     class Program
     {
+        //Given a position of a knight on the standard chessboard, find the number 
+        //of different moves the knight can perform.
+        //The knight can move to a square that is two squares horizontally and one square vertically,
+        //or two squares vertically and one square horizontally away from it.
+        //The complete move therefore looks like the letter L.
+        //Check out the image below to see all valid moves for a knight piece that is 
+        //placed on one of the central squares.
         static void Main(string[] args)
         {
             string cell = "a1";
