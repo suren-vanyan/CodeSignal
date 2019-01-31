be74b51005262c092f649a80aa72ea38e3b4be70
suren-vanyan <vanyansuren@gmail.com>
2019-01-31 23:53:55 +0400

    lineEncoding

---
+++ Intro/lineEncoding/lineEncoding/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace lineEncoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ssiiggkooo";
+            Console.WriteLine(lineEncoding(s));
+        }
+
+        static string lineEncoding(string s)
+        {
+           
+
+            char temp = s[0];
+            string result = string.Empty;
+            int counter = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (temp == s[i])
+                    counter++;
+                else
+                {
+                    if (counter > 1)
+                        result += counter.ToString() + temp;
+                    else
+                        result += temp;
+                    counter = 1;
+                    temp = s[i];
+                }
+            }
+
+            if (counter > 1) result += counter.ToString() + temp;
+            else result += temp;
+            return result;
+
+        }
+    }
+}
